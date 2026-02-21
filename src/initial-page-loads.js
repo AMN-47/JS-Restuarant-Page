3019efde6092d42f4113fc401b1c4992eb535190
AMN-47 <akhilnagar57@gmail.com>
2026-02-21 21:52:10 +0200

    Added import for image and implemented in code, image loaded sucessfully

--- src/initial-page-loads.js
+++ src/initial-page-loads.js
@@ -1,7 +1,30 @@
+import wineImage from "./Images/duynod-wine-8346641_1920.jpg"
+
 export function initialPageLoad() {
     const contentDiv = document.querySelector("#content");
     const heading = document.createElement("h1");
     heading.textContent = "Welcome to the abyss";
     heading.classList.add("landing-title");
     contentDiv.appendChild(heading);
+
+    const topImage = document.createElement("img");
+    topImage.classList.add("top-image");
+    topImage.src = wineImage;
+    topImage.alt = "Image for top";
+    contentDiv.appendChild(topImage);
+
+    const para1 = document.createElement("p");
+    para1.classList.add("landing-page-copy");
+    para1.textContent = "This is an upscale restuarant";
+    contentDiv.appendChild(para1);
+
+    const para2 = document.createElement("p");
+    para2.classList.add("landing-page-copy");
+    para2.textContent = "The requirement is business casual";
+    contentDiv.appendChild(para2);
+
+    const para3 = document.createElement("p");
+    para3.classList.add("landing-page-copy");
+    para3.textContent = "Thanks, Management"
+    contentDiv.appendChild(para3);
 }
