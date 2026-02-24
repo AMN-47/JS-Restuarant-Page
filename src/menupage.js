4269ee475a96bf629a9e95f8755f66a8a9e90d3f
AMN-47 <akhilnagar57@gmail.com>
2026-02-24 18:55:44 +0200

    added header to menupage

--- src/menupage.js
+++ src/menupage.js
@@ -4,4 +4,21 @@ export function menuPageLoad() {
     const pageHeader = document.createElement("div");
     pageHeader.classList.add("page-header");
 
+    const pageTitle = document.createElement("h1");
+    pageTitle.classList.add("page-title");
+    pageTitle.textContent = "Our Menu";
+
+    const pageLine = document.createElement("span");
+    pageLine.classList.add("page-title-line");
+
+    const pageSubtitle = document.createElement("p");
+    pageSubtitle.classList.add("page-subtitle");
+    pageSubtitle.textContent = "Thoughtfully Crafted. Seasonally inspired.";
+
+    pageHeader.appendChild(pageTitle);
+    pageHeader.appendChild(pageLine);
+    pageHeader.appendChild(pageSubtitle)
+    contentDiv.appendChild(pageHeader);
+
+    //Menu Items
 }
