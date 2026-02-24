d9bbb2279d91478a4114bad89c1d47e1f246822f
AMN-47 <akhilnagar57@gmail.com>
2026-02-24 11:38:24 +0200

    inital commit for menu page

---
+++ src/menupage.js
@@ -0,0 +1,7 @@
+export function menuPageLoad() {
+    const contentDiv = document.querySelector("#content");
+
+    const pageHeader = document.createElement("div");
+    pageHeader.classList.add("page-header");
+
+}
